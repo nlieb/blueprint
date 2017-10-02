cf7e62284b712c1a3cffec3d22259451ef6fb94a
eyesniper2 <eyesniper2@rayfall.net>
2017-10-01 22:54:25 -0600

    Added Info Textbox back into app

--- src/App.js
+++ src/App.js
@@ -14,21 +14,13 @@ class App extends Component {
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
                 </head>
                 <BlueprintNavBar/>
-<<<<<<< HEAD
-                <InterestChart
-                    width={960}
-                    height={500}
-
-                />
-                <BlueprintInfoTextbox/>
-=======
                 <div className="content">
                     <InterestChart
                         width={960}
                         height={500}
                     />
                 </div>
->>>>>>> f14b28087d4eb5c980e44ddf583115da287d4be8
+                <BlueprintInfoTextbox/>
             </div>
         );
     }
