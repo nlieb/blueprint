6513f4321224bb82b82a06fddb381a43fcb55b53
eyesniper2 <eyesniper2@rayfall.net>
2017-10-01 22:52:08 -0600

    First pass on info text box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import BlueprintNavBar from './components/NavBar';
 import InterestChart from './components/InterestChart';
+import BlueprintInfoTextbox from './components/InfoTextbox';
 
 class App extends Component {
     render() {
@@ -18,6 +19,7 @@ class App extends Component {
                     height={500}
 
                 />
+                <BlueprintInfoTextbox/>
             </div>
         );
     }
---
+++ src/components/InfoTextbox.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col } from 'react-bootstrap';
+
+const BlueprintInfoTextbox = (props) => {
+    return (
+        <Row className="info-row">
+            <Col md={6} mdOffset={6}>
+                <p>First Pass on text</p>
+            </Col>
+        </Row>
+    );
+};
+
+export default BlueprintInfoTextbox;
