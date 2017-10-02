527244fe7f0c1c374fc123367a3bc75073b54785
James Hryniw <jhryniw@gmail.com>
2017-10-01 22:25:02 -0600

    Basic navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,8 @@ import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const BlueprintNavBar = (props) => {
     return (
-        <Navbar inverse collapseOnSelect>
+        <Navbar inverse collapseOnSelect
+            style={{ position: "absolute", top: 0, left: 0, width: "100%" }}>
             <Navbar.Header>
                 <Navbar.Brand>
                     <a href="#">Blueprint</a>
@@ -14,24 +15,24 @@ const BlueprintNavBar = (props) => {
                 <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
-                <Nav pullRight>
-                    <NavDropdown eventKey={3} title="Why Blueprint?" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Overview</MenuItem>
-                        <MenuItem eventKey={3.2}>Smart Investing</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <Nav pullRight style={{ paddingRight: 20 }}>
+                    <NavDropdown eventKey={1} title="Why Blueprint?" id="basic-nav-dropdown">
+                        <MenuItem eventKey={1.1}>Overview</MenuItem>
+                        <MenuItem eventKey={1.2}>Smart Investing</MenuItem>
+                        <MenuItem eventKey={1.3}>Something else here</MenuItem>
                         <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        <MenuItem eventKey={1.3}>Separated link</MenuItem>
                     </NavDropdown>
-                    <NavDropdown eventKey={3} title="Portfolio" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                    <NavDropdown eventKey={2} title="Portfolio" id="basic-nav-dropdown">
+                        <MenuItem eventKey={2.1}>Action</MenuItem>
+                        <MenuItem eventKey={2.2}>Another action</MenuItem>
+                        <MenuItem eventKey={2.3}>Something else here</MenuItem>
                         <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        <MenuItem eventKey={2.3}>Separated link</MenuItem>
                     </NavDropdown>
-                    <NavItem eventKey={1} href="#">FAQ's</NavItem>
-                    <NavItem eventKey={2} href="#">Login</NavItem>
-                    <NavItem eventKey={2} href="#">Start Investing</NavItem>
+                    <NavItem eventKey={3} href="#">FAQ's</NavItem>
+                    <NavItem eventKey={4} href="#">Login</NavItem>
+                    <NavItem eventKey={5} href="#">Start Investing</NavItem>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
